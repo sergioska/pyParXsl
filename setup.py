06705c9598e6991a4bbfbf514cc141997dfcf5d1
Ska <sergiosicari@gmail.com>
2014-05-03 17:42:47 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+  name="pyParXsl",
+  version="*",
+  description="Toolkit for python parser applications xslt transformation language based",
+  author="Sergio Sicari",
+  author_email="sergiosicari@gmail.com",
+  url="http://github.com/sergioska/pyParXsl/tree/master",
+  download_url="http://github.com/sergioska/PyParXsl/tree/master",
+  packages=['pyparxsl'],
+  zip_safe = False, # we include templates and tests
+  install_requires=['lxml==3.1.2', 'BeautifulSoup==4.3.2', 'pycurl==7.19.3.1'],
+  )
