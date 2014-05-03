1d9b9212db498108175e4fbecc0c5ed438a34015
Ska <sergiosicari@gmail.com>
2014-05-03 19:26:44 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
   author_email="sergiosicari@gmail.com",
   url="http://github.com/sergioska/pyParXsl/tree/master",
   download_url="http://github.com/sergioska/PyParXsl/tree/master",
-  packages=['pyparxsl'],
   zip_safe = False, # we include templates and tests
   install_requires=['lxml==3.1.2', 'BeautifulSoup==4.3.2', 'pycurl==7.19.3.1'],
   )
